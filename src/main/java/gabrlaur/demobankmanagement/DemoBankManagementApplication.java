1292883885d4e2aeb7f0d68acbcd7e5016a703d5
gabrlaur <45432522+gabrlaur@users.noreply.github.com>
2022-06-07 20:39:46 +0300

    initial commit
    created project

---
+++ src/main/java/gabrlaur/demobankmanagement/DemoBankManagementApplication.java
@@ -0,0 +1,13 @@
+package gabrlaur.demobankmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBankManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBankManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gabrlaur/demobankmanagement/DemoBankManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package gabrlaur.demobankmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBankManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
